214c63ad9ed7d2579344832561564666ceecee60
tsmadzharov21 <tsmadzharov21@codingburgas.bg>
2023-11-08 22:09:29 +0200

    Fix errors

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,14 +1,8 @@
 #include "raylib.h"
 #include "atom.h"
-#include <Windows.h>
-#include <mmsystem.h>
 #include <iostream>
 
 int main() {
-
-
-    PlaySound(TEXT("1107.wav"), NULL, SND_FILENAME | SND_SYNC);
-    return 0;
 
     InitGame();
 
