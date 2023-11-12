317bd3f4bebffb4729348481a648c0e0cd2a88d3
tsmadzharov21 <tsmadzharov21@codingburgas.bg>
2023-11-12 22:08:56 +0200

    Fixed bugs

--- src/game/mainMenu.cpp
+++ src/game/mainMenu.cpp
@@ -1,5 +1,5 @@
 #include "atom.h"
-void storyline();
+
 
 void mainMenu() {
     const int screenWidth = 1280;
@@ -10,34 +10,16 @@ void mainMenu() {
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_DOWN)) {
-            selectedOption = (selectedOption + 1) % 5;
+            selectedOption = (selectedOption + 1) % 2;
         }
         else if (IsKeyPressed(KEY_UP)) {
-            if (selectedOption == 0) {
-                selectedOption = 4;
-            }
-            else {
-                selectedOption = (selectedOption - 1) % 5;
-            }
+            selectedOption = (selectedOption - 1 + 2) % 2;
         }
-        
+
 
         if (IsKeyPressed(KEY_ENTER)) {
-
-            if (selectedOption == 1) {
-                storyline();
-            }
-            else if (selectedOption == 2){
-                exit(1);
-
-            }
-            else if (selectedOption == 3) {
-                exit(1);  
-
-            }
-            else if (selectedOption == 4) {
-                exit(1);
-
+            if (selectedOption == 0) {
+                return;
             }
             else {
                 CloseWindow();
@@ -46,15 +28,11 @@ void mainMenu() {
         }
 
         BeginDrawing();
-        DrawTextureEx(menuTexture, { 0,0 }, 0, 0.69, WHITE);
+        DrawTextureEx(menuTexture, { 0,0 }, 0, 0.7, WHITE);
 
         // Draw the lobby options
-        DrawText("Play Game", screenWidth / 2 - 80, screenHeight / 2 - 100, 30, (selectedOption == 0) ? GREEN : WHITE);
-        DrawText("Storyline", screenWidth / 2 - 80, screenHeight / 2 - 40, 30, (selectedOption == 1) ? GREEN : WHITE);
-        DrawText("Rules", screenWidth / 2 - 80, screenHeight / 2 + 20, 30, (selectedOption == 2) ? GREEN : WHITE);
-        DrawText("Credits", screenWidth / 2 - 80, screenHeight / 2 + 80, 30, (selectedOption == 3) ? GREEN : WHITE);
-        DrawText("Quit", screenWidth / 2 - 80, screenHeight / 2 + 140, 30, (selectedOption == 4) ? GREEN : WHITE);
-
+        DrawText("Play Game", screenWidth / 2 - 100, screenHeight / 2 - 30, 30, (selectedOption == 0) ? RED : DARKGRAY);
+        DrawText("Quit", screenWidth / 2 - 100, screenHeight / 2 + 30, 30, (selectedOption == 1) ? RED : DARKGRAY);
 
         EndDrawing();
     }
